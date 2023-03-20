963b576d757d58fbcee41fc1445208dde9ff7422
wendy0626-cli <77438845+wendy0626-cli@users.noreply.github.com>
2023-03-20 10:40:57 +0800

    Update transfervalue.js

--- 01_MultiCall/transfervalue.js
+++ 01_MultiCall/transfervalue.js
@@ -3,9 +3,9 @@ const {ethers} = require("hardhat");
 
 async function main() {
   // We get the contract to deploy
-//   const hAddress = '0x920507d260A7Eb5F6a4A862Ef25D6a78c1A2179C';
-//   const iAddress = '0x8a03fEf993b04eF0ECD386fDa4e78998BC2A7506';
-//   const jAddress = '0x4933A8caA4B1f51e4c144a811B5e720d30848589';
+//   const hAddress = '';
+//   const iAddress = '';
+//   const jAddress = '';
   const hAddress = '0xb140D9EaDFA587b7254EaAa07eD42Fd0DDf4A963';
   const iAddress = '0xd1e40cB517fFB8ebD0034d185A2130b9Ad249BDE';
   const jAddress = '0x308B0aC0E903a5190E3Df90Da187118A0Fd56553';
